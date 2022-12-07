fab4655c06b54b82289290518a70113ce4f47a17
rafal <zebraf.dev@gmail.com>
2022-12-07 21:42:47 +0100

    day7 refactored

--- Day7/day7.py
+++ Day7/day7.py
@@ -12,46 +12,44 @@ class DirNode:
 
 
 class FilesystemTree:
-    def __init__(self):
+    def __init__(self, lines):
         self.root = DirNode('root')
+        self.build_filesystem_tree(self.root, lines)
     
     def mkdir(self, name, prev):
         dir = DirNode(name, prev)
         return dir
 
-    def build_filesystem_tree(self, lines):
-        
-        def build(curr_dir, lines):
-            if lines[0][0] == '$':
-                i = 0
-                if lines[i][1] == 'ls':
-                    i = 1
-                    while i < len(lines) and (lines[i][0].isnumeric() or lines[i][0] == 'dir'):
-                        if lines[i][0] == 'dir':
-                            curr_dir.sub_dirs[lines[i][1]] = self.mkdir(lines[i][1], curr_dir)
-                        else:
-                            curr_dir.files.append(lines[i])
-                        i += 1
-                                      
-                if i < len(lines):
-                    if lines[i][2] == '/':
-                        build(self.root, [*lines[i+1:]])
-                    elif lines[i][2] == '..':
-                        prev_directory = curr_dir.prev
-                        build(prev_directory, [*lines[i+1:]])
+    def build_filesystem_tree(self, curr_dir, lines):
+        if not lines:
+            return
+        if lines[0][0] == '$':
+            i = 0
+            if lines[i][1] == 'ls':
+                i = 1
+                while i < len(lines) and (lines[i][0].isnumeric() or lines[i][0] == 'dir'):
+                    if lines[i][0] == 'dir':
+                        curr_dir.sub_dirs[lines[i][1]] = self.mkdir(lines[i][1], curr_dir)
                     else:
-                        next_directory = curr_dir.sub_dirs[lines[i][2]]
-                        next_directory.prev = curr_dir
-                        build(next_directory, [*lines[i+1:]])
-            else:
-                raise Exception("Wrong input!")
+                        curr_dir.files.append(lines[i])
+                    i += 1
+                                    
+            if i < len(lines):
+                if lines[i][2] == '/':
+                    self.build_filesystem_tree(self.root, [*lines[i+1:]])
+                elif lines[i][2] == '..':
+                    prev_directory = curr_dir.prev
+                    self.build_filesystem_tree(prev_directory, [*lines[i+1:]])
+                else:
+                    next_directory = curr_dir.sub_dirs[lines[i][2]]
+                    next_directory.prev = curr_dir
+                    self.build_filesystem_tree(next_directory, [*lines[i+1:]])
+        else:
+            raise Exception("Wrong input!")
 
-        build(self.root, lines[1:])
-        return self.root
 
-
-file_system = FilesystemTree()
-root = file_system.build_filesystem_tree(lines)
+file_system = FilesystemTree(lines)
+root = file_system.root
 
 
 class DaySeven:
@@ -93,4 +91,3 @@ class DaySeven:
 
 
 day_seven = DaySeven(root)
-
