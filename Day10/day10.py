fb51e86801eeecc3f530c0c7a8c522e2708f2d33
rafal <zebraf.dev@gmail.com>
2022-12-18 21:00:18 +0100

    day10 part2, part1 refactored

--- Day10/day10.py
+++ Day10/day10.py
@@ -5,19 +5,14 @@ with open('input.txt') as f:
 def part_one(program):
     x = 1
     singal_strength = 0
-    
     cycle = 1
     important_cycle = 20
     multiplicator = 1
-    
     addx = None
     for instruction in program:
         if len(instruction) == 2:
             addx = int(instruction[1])
-            cycle += 1
-        else:
-            cycle += 1
-        
+        cycle += 1
         while True:
             if cycle == important_cycle * multiplicator:
                 singal_strength +=  x * (important_cycle * multiplicator)
@@ -28,5 +23,42 @@ def part_one(program):
                 addx = None
                 continue
             break
-    
+
     return singal_strength
+
+def part_two(program):  
+    
+    def increment_cycle(rows, cycle, x, addx=None):
+        cycle += 1
+        if cycle - 1 in (x - 1, x, x + 1):
+            rows[cycle-1] = '#'
+        if cycle in (40, 80, 120, 160, 200, 240):
+            x += 40
+        if addx:
+            x += addx
+        return cycle, x
+
+    rows = ['.' for _ in range(240)]
+
+    # sprite position
+    x = 1
+    cycle = 0
+    addx = None
+    for instruction in program:
+        if len(instruction) == 2:
+            addx = int(instruction[1])
+        
+        cycle, x = increment_cycle(rows, cycle, x)
+        
+        if addx:
+            cycle, x = increment_cycle(rows, cycle, x, addx)
+            addx = None
+
+    left = 0
+    right = 40
+    for _ in range(6):
+        print("".join(rows[left:right]))
+        left += 40
+        right += 40
+    
+    return rows
